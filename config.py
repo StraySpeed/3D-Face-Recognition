5f7d5d6d01879b7139a7afaa3212395b6f549d1d
StraySpeed(lab) <yo2189@cau.ac.kr>
2026-03-24 20:08:42 +0900

    [Res] config.py
    - config.py에 맞게 모델 수정

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-import torch
 import os
+import torch
 
 # 현재 프로젝트의 절대 경로
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,9 +9,11 @@ CONFIG = {
     # 1. 데이터 및 경로 설정
     "PATH": {
         "data_root": os.path.join(BASE_DIR, "dataset/umbdb"),   # 학습용 데이터 경로
-        "checkpoint_dir": os.path.join(BASE_DIR, "checkpoints1"),    # 체크포인트 경로
+        "checkpoint_dir": os.path.join(BASE_DIR, "checkpoints2"),    # 체크포인트 경로
         "gallery_dir": os.path.join(BASE_DIR, "dataset_matching/umbdb_unpreprocessed"), # 매칭할 얼굴 데이터 경로
-        "gallery_storage": os.path.join(BASE_DIR, "gallery_storage1/umbdb"), # 미리 저장된 얼굴 데이터 경로
+        "gallery_storage": os.path.join(BASE_DIR, "gallery_storage2/umbdb"), # 미리 저장된 얼굴 데이터 경로
+        "gallery_storage_enc": os.path.join(BASE_DIR, "gallery_storage2_enc/umbdb"), # 미리 저장된 얼굴 데이터 경로
+        "secret.context": os.path.join(BASE_DIR, "gallery_storage2_enc/secret.context")
     },
     
     # 2. 학습(Training) 하이퍼파라미터
@@ -29,10 +31,12 @@ CONFIG = {
         "num_classes": 143, # 분류 개수 (모델 학습에만 필요한 파라미터. feature 추출에는 상관 X)
         "input_channel": 3,  # 기본 좌표(x,y,z)
         "num_points": 5000,  # 원본 입력 점 개수
+        "feature_dim": 512    # 최종 특징 차원
     },
     
     # 4. Set Abstraction (RSConv) 계층별 파라미터 (4 -> 2 -> 1 분할 구조)
     # [npoint, radius, nsample, in_channel, out_channel, hidden_channel]
+
     "SA_PARAMS": [
         {'npoint': 512, 'radius': 0.10, 'nsample': 32, 'in_ch': 3,   'out_ch': 64,  'hid_ch': 16},  # SA1
         {'npoint': 512, 'radius': 0.20, 'nsample': 32, 'in_ch': 64,  'out_ch': 128, 'hid_ch': 32},  # SA2
@@ -43,7 +47,6 @@ CONFIG = {
     # 5. 매칭(Matching) 설정
     "MATCHING": {
         "threshold": 0.7
-    },
-    
-    "DEVICE" : 'cuda' if torch.cuda.is_available() else 'cpu'
+    }, 
+    "DEVICE": 'cuda' if torch.cuda.is_available() else 'cpu'
 }
--- model/pointface_encoder.py
+++ model/pointface_encoder.py
@@ -21,8 +21,8 @@ class PointFaceEncoder(nn.Module):
         
         # Final Fully Connected Layer 
         self.fc = nn.Sequential(
-            nn.Linear(512, 512),
-            nn.BatchNorm1d(512),
+            nn.Linear(sa_params[-1]["out_ch"], CONFIG["MODEL"]["feature_dim"]),
+            nn.BatchNorm1d(CONFIG["MODEL"]["feature_dim"]),
         )
 
     def forward(self, x, pre_data=None):
@@ -36,45 +36,54 @@ class PointFaceEncoder(nn.Module):
         precomputed_indices = pre_data['indices']
         
         # 입력 데이터를 4등분 후 1단계 처리
-        xyz_batch1 = xyz.view(B, 3, 4, 1250).transpose(1, 2).reshape(-1, 3, 1250)
+        num_points = CONFIG["MODEL"]["num_points"] // 4
+        xyz_batch1 = xyz.view(B, 3, 4, num_points).transpose(1, 2).reshape(-1, 3, num_points)
         if features is not None:
-            feat_batch1 = features.view(B, features.shape[1], 4, 1250).transpose(1, 2).reshape(-1, features.shape[1], 1250)
+            feat_batch1 = features.view(B, features.shape[1], 4, num_points).transpose(1, 2).reshape(-1, features.shape[1], num_points)
         else:
             feat_batch1 = None
 
-        c1 = precomputed_centroids['s1'].view(-1, 3, 512) if precomputed_centroids else None
-        i1 = precomputed_indices['s1'].view(-1, 512, 32)  if precomputed_indices else None
+        s1_points = CONFIG["SA_PARAMS"][0]["npoint"]
+        s1_sample = CONFIG["SA_PARAMS"][0]["nsample"]
+        c1 = precomputed_centroids['s1'].view(-1, 3, s1_points) if precomputed_centroids else None
+        i1 = precomputed_indices['s1'].view(-1, s1_points, s1_sample)  if precomputed_indices else None
         l1_xyz, l1_points = self.sa1(xyz_batch1, feat_batch1, c1, i1)
 
         # 4 -> 2개씩 붙여서 2단계 처리
-        l1_x_unflat = l1_xyz.view(B, 4, 3, 512)
+        l1_x_unflat = l1_xyz.view(B, 4, 3, s1_points)
         xyz_12 = torch.cat([l1_x_unflat[:, 0], l1_x_unflat[:, 1]], dim=2) # (B, 3, 1024)
         xyz_34 = torch.cat([l1_x_unflat[:, 2], l1_x_unflat[:, 3]], dim=2)
-        xyz_batch2 = torch.stack([xyz_12, xyz_34], dim=1).view(-1, 3, 1024) # (2B, 3, 1024)
+        xyz_batch2 = torch.stack([xyz_12, xyz_34], dim=1).view(-1, 3, s1_points * 2) # (2B, 3, 1024)
         
-        l1_f_unflat = l1_points.view(B, 4, l1_points.shape[1], 512)
+        l1_f_unflat = l1_points.view(B, 4, l1_points.shape[1], s1_points)
         feat_12 = torch.cat([l1_f_unflat[:, 0], l1_f_unflat[:, 1]], dim=2)
         feat_34 = torch.cat([l1_f_unflat[:, 2], l1_f_unflat[:, 3]], dim=2)
-        feat_batch2 = torch.stack([feat_12, feat_34], dim=1).view(-1, l1_points.shape[1], 1024)
+        feat_batch2 = torch.stack([feat_12, feat_34], dim=1).view(-1, l1_points.shape[1], s1_points * 2)
         
-        c2 = precomputed_centroids['s2'].view(-1, 3, 512) if precomputed_centroids else None
-        i2 = precomputed_indices['s2'].view(-1, 512, 32)  if precomputed_indices else None
+        s2_points = CONFIG["SA_PARAMS"][1]["npoint"]
+        s2_sample = CONFIG["SA_PARAMS"][1]["nsample"]
+        c2 = precomputed_centroids['s2'].view(-1, 3, s2_points) if precomputed_centroids else None
+        i2 = precomputed_indices['s2'].view(-1, s2_points, s2_sample)  if precomputed_indices else None
         l2_xyz, l2_points = self.sa2(xyz_batch2, feat_batch2, c2, i2)
         
         # 2 -> 1로 최종 처리
-        l2_x_unflat = l2_xyz.view(B, 2, 3, 512)
+        l2_x_unflat = l2_xyz.view(B, 2, 3, s2_points)
         l2_xyz_all = torch.cat([l2_x_unflat[:, 0], l2_x_unflat[:, 1]], dim=2)
         
-        l2_f_unflat = l2_points.view(B, 2, l2_points.shape[1], 512)
+        l2_f_unflat = l2_points.view(B, 2, l2_points.shape[1], s2_points)
         l2_points_all = torch.cat([l2_f_unflat[:, 0], l2_f_unflat[:, 1]], dim=2)
         
-        c3 = precomputed_centroids['s3'].view(-1, 3, 512) if precomputed_centroids else None
-        i3 = precomputed_indices['s3'].view(-1, 512, 32)  if precomputed_indices else None
+        s3_points = CONFIG["SA_PARAMS"][2]["npoint"]
+        s3_sample = CONFIG["SA_PARAMS"][2]["nsample"]
+        c3 = precomputed_centroids['s3'].view(-1, 3, s3_points) if precomputed_centroids else None
+        i3 = precomputed_indices['s3'].view(-1, s3_points, s3_sample)  if precomputed_indices else None
         l3_xyz, l3_points = self.sa3(l2_xyz_all, l2_points_all, c3, i3)
 
+        s4_points = CONFIG["SA_PARAMS"][3]["npoint"]
+        s4_sample = CONFIG["SA_PARAMS"][3]["nsample"]
         # 이게 필요한가?는 고민해볼 것
-        c4 = precomputed_centroids['s4'].view(-1, 3, 256) if precomputed_centroids else None
-        i4 = precomputed_indices['s4'].view(-1, 256, 32)  if precomputed_indices else None
+        c4 = precomputed_centroids['s4'].view(-1, 3, s4_points) if precomputed_centroids else None
+        i4 = precomputed_indices['s4'].view(-1, s4_points, s4_sample)  if precomputed_indices else None
         l4_xyz, l4_points = self.sa4(l3_xyz, l3_points, c4, i4)
         
         # Global Max Pooling: (B, 512, 256) -> (B, 512)
